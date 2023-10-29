05b133b5d67df9fdf6da8235d8dae868e2480651
Yevhen <beetroot093@gmail.com>
2023-10-29 19:29:44 +0100

    Step 5

--- EFCore1/Context/EFCoreContext.cs
+++ EFCore1/Context/EFCoreContext.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore1.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCore1.Context;
 
-public class EFCoreContext: DbContext
+public class EFCoreContext : DbContext
 {
-    public EFCoreContext()
-    {
-    }
+    public DbSet<User> Users { get; private set; }
 
-    public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options)
+    public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>().HasKey(x => x.Id);
+        modelBuilder.Entity<User>().Property(b => b.Name).IsRequired();
+        modelBuilder.Entity<User>().Property(b => b.Surname).IsRequired();
+
+        modelBuilder.Entity<User>().Property(x => x.Birthday)
+            .HasConversion(new ValueConverter<DateOnly?, DateTime?>(
+                x => x.HasValue ? x.Value.ToDateTime(TimeOnly.MinValue) : null,
+                y => y.HasValue ? DateOnly.FromDateTime(y.Value) : null)
+            );
     }
 }
---
+++ EFCore1/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateOnly? Birthday { get; set; }
+    }
+}
