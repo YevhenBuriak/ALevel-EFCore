b3cac4acf47c79f519234fa0623a484d30f1b5e8
Yevhen <beetroot093@gmail.com>
2023-10-29 18:48:51 +0100

    Step 3

---
+++ EFCore1/Context/EFCoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore1.Context;
+
+public class EFCoreContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionString =
+            "Data Source=localhost;Initial Catalog = master;user id=sa;password=passwordX123;Integrated Security=False;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
---
+++ EFCore1/Controllers/EfTestController.cs
@@ -0,0 +1,24 @@
+﻿using EFCore1.Context;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EfTestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> TestConnection()
+        {
+            var dbContext = new EFCoreContext();
+
+            if (dbContext.Database.CanConnect())
+            {
+                return Ok(dbContext.Database.ProviderName);
+            }
+
+            return BadRequest();
+        }
+    }
+}
