d011437f49d490a09a8d600700eb3dcf96977f46
Yevhen <beetroot093@gmail.com>
2023-11-01 22:58:18 +0100

    Step 1

---
+++ EFCore1/Repository/UsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using EFCore1.Context;
+using EFCore1.Models;
+
+namespace EFCore1.Repository
+{
+    public class UsersRepository
+    {
+        private readonly EFCoreContext _dbContext;
+
+        public UsersRepository(EFCoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
