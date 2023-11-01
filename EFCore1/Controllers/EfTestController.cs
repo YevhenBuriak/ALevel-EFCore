d751156f5c16d43ae16b169a9b307c1bec75299a
Yevhen <beetroot093@gmail.com>
2023-11-01 23:39:10 +0100

    Step 2.1

--- EFCore1/Controllers/EfTestController.cs
+++ EFCore1/Controllers/EfTestController.cs
@@ -11,28 +11,55 @@ namespace EFCore1.Controllers
     [ApiController]
     public class EfTestController : ControllerBase
     {
-        private readonly EFCoreContext _dbcontext;
+        private readonly EFCoreContext _dbContext;
 
         public EfTestController(EFCoreContext dbcontext)
         {
-            _dbcontext = dbcontext;
+            _dbContext = dbcontext;
         }
 
         [HttpGet("connection-test")]
         public async Task<ActionResult> TestConnection()
         {
-            if (_dbcontext.Database.CanConnect())
+            if (_dbContext.Database.CanConnect())
             {
-                return Ok(_dbcontext.Database.ProviderName);
+                return Ok(_dbContext.Database.ProviderName);
             }
 
             return BadRequest();
         }
 
+        [HttpGet("change-tracker-test")]
+        public async Task<ActionResult> TestChangeTracking()
+        {
+            // 1. Change tracker
+            _ = _dbContext.ChangeTracker;
+
+            // 2. Track entity
+            _dbContext.Attach(new User { Id = 1 });
+
+            // 4. On retrieve (need to have at least 2 users in db =))
+            var users = await _dbContext.Users.ToListAsync();
+
+            // 5. Change
+            users.First().Name = "Change tracked";
+
+            // 6. Detach one
+            _dbContext.Entry(users.Last()).State = EntityState.Detached;
+            
+            // 6. Detach all
+            _dbContext.ChangeTracker.Clear();
+
+            // 7. As no tracking
+            var deatachedUsers = await _dbContext.Users.AsNoTracking().ToListAsync();
+
+            return Ok();
+        }
+
         [HttpGet("users")]
         public async Task<ActionResult> GetUsers()
         {
-            var users = await _dbcontext.Users.ToListAsync();
+            var users = await _dbContext.Users.ToListAsync();
 
             return Ok(users);
         }
@@ -40,7 +67,7 @@ namespace EFCore1.Controllers
         [HttpPost]
         public async Task<ActionResult> CreateUser(User user)
         {
-            var repo = new UsersRepository(_dbcontext);
+            var repo = new UsersRepository(_dbContext);
             await repo.Insert(user);
 
             return Ok();
@@ -49,7 +76,7 @@ namespace EFCore1.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(User user)
         {
-            var repo = new UsersRepository(_dbcontext);
+            var repo = new UsersRepository(_dbContext);
             await repo.Update(user);
 
             return Ok();
@@ -58,7 +85,7 @@ namespace EFCore1.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteUser([FromRoute] int id)
         {
-            var repo = new UsersRepository(_dbcontext);
+            var repo = new UsersRepository(_dbContext);
             await repo.Delete(id);
 
             return Ok();
